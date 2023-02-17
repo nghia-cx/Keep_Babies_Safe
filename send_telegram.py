27605e12fc2d338e881443aa60a83b16db39a603
nghia-cx <96898910+nghia-cx@users.noreply.github.com>
2023-02-17 17:07:24 +0700

    Update send_telegram.py

--- send_telegram.py
+++ send_telegram.py
@@ -3,7 +3,7 @@ import telegram
 def send_warning(photo_path="alert.png"):
     try:
         bot = telegram.Bot(token='your_token')
-        bot.send_photo(chat_id='5923323553',photo=open(photo_path, "rb"), caption="Baby Dangerous!!") 
+        bot.send_photo(chat_id='your_id',photo=open(photo_path, "rb"), caption="Baby Dangerous!!") 
         print("Send Success!")
     except Exception as ex:
         print("Can not send warning! ", ex)
